15e727e8b3d67ab31777926caf0b5d87cbf6c9ee
Nat Young <nat.young7@gmail.com>
2016-03-10 17:54:58 +1100

    Hello, team, this is the initial commit of the app onto github

---
+++ src/Ikanz.js
@@ -0,0 +1,16 @@
+'use strict';
+import React, {
+    Component
+} from 'react-native';
+
+class Ikanz extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <CardDeckContainer />
+        )
+    }
+}
